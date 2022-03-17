81e42690177d24b8d93a46ec7b1785bf97e1a8cd
safaosmank <safakorne@gmail.com>
2022-03-17 22:04:16 +0300

    modified

--- doorlock_mqtt/doorlock_mqtt.ino
+++ doorlock_mqtt/doorlock_mqtt.ino
@@ -1,17 +1,24 @@
-
+//#ifdef ESP32
+//#include <WiFi.h>
+//#else
+//#include <ESP8266WiFi.h>
+//#endif
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <ArduinoJson.h>
 #include <Keypad.h>
-#include <WiFi.h>
+//#include <WiFi.h>
 #include <PubSubClient.h>
 
 #define ROW_NUM     4 // four rows
-#define COLUMN_NUM  4// three columns
-#define relay 26
+#define COLUMN_NUM  4// three columns#define relay 26
 #define buzzer 27
-#define debug(x) 
+#define debug(x)
 #define debugln(x)
-
+const int relay = 26;
 byte pin_column[COLUMN_NUM] = {23, 22, 3, 21}; // GIOP18, GIOP5, GIOP17, GIOP16 connect to the row pins
 byte pin_rows[ROW_NUM] = {19, 18, 5,17};  // GIOP4, GIOP0, GIOP2 connect to the column pins
+
 
 char keys[ROW_NUM][COLUMN_NUM] = {
   {'1', '2', '3','A'},
@@ -22,14 +29,74 @@ char keys[ROW_NUM][COLUMN_NUM] = {
 
 Keypad keypad = Keypad( makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM );
 
-const String password = "7890"; // change your password here
+const String password = "7777"; // change your password here
 String input_password;
 
 
 const char* ssid = "";
-const char* passwd ="";
+const char* passwd = "";
 const char* mqtt_server = "";
 
+// Initialize Telegram BOT
+#define BOTtoken ""  // your Bot Token (Get from Botfather)
+//Enter either group ID or the chat ID
+#define CHAT_ID ""
+WiFiClientSecure botclient;
+UniversalTelegramBot bot(BOTtoken, botclient);
+// Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
+bool relayState = LOW;
+bool buzzerState = LOW;
+// Handle new receive messages
+void handleNewMessages(int numNewMessages) {
+  Serial.println("handleNewMessages");
+  Serial.println(String(numNewMessages));
+  for (int i=0; i<numNewMessages; i++) {
+    // Chat id of the requester
+    String chat_id = String(bot.messages[i].chat_id);
+    if (chat_id != CHAT_ID){
+      bot.sendMessage(chat_id, "Unauthorized user", "");
+      continue;
+      }
+    // Print the received message
+    String text = bot.messages[i].text;
+    Serial.println(text);
+    String from_name = bot.messages[i].from_name;
+    if (text == "/start") {
+      String welcome = "Welcome, " + from_name + ".\n";
+      welcome += "Use the following commands to control your outputs.\n\n";
+      welcome += "/open to open door \n";
+      welcome += "/close to close door \n";
+      welcome += "/state to request current door state \n";
+      bot.sendMessage(chat_id, welcome, "");
+    }
+    if (text == "/open") {
+      bot.sendMessage(chat_id, "door state set to open", "");
+      relayState = HIGH;
+      digitalWrite(relay, relayState);
+      delay(5000);
+      relayState = LOW;
+      digitalWrite(relay, relayState);
+    }
+    if (text == "/close") {
+      bot.sendMessage(chat_id, "door state set to close", "");
+      relayState = LOW;
+      digitalWrite(relay, relayState);
+    }
+
+    if (text == "/state") {
+      if (digitalRead(relay)){
+        bot.sendMessage(chat_id, "door is open", "");}
+      else
+        //bot.sendMessage(chat_id, "blueLED is OFF", "");
+       {
+        bot.sendMessage(chat_id, "door is closed", "");
+       }
+
+    }
+}
+}
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -47,6 +114,7 @@ void setup_wifi() {
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, passwd);
+  WiFi.setSleep(false);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -70,10 +138,10 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.println();
 
   // open the lock if an 1 was received as first character
-  
+
  if ((char)payload[0] == '0') {
-   
-    digitalWrite(relay, LOW);   
+
+    digitalWrite(relay, LOW);
    digitalWrite(buzzer,HIGH);
     client.publish("outDoor","closed");
    delay(100);
@@ -82,19 +150,19 @@ void callback(char* topic, byte* payload, unsigned int length) {
     digitalWrite(buzzer,HIGH);
     delay(100);
     digitalWrite(buzzer,LOW);
-     
-  } 
-  
+
+  }
+
   else if ((char)payload[0] == '1'){
    digitalWrite(relay,HIGH);
    digitalWrite(buzzer, HIGH);
-   client.publish("outDoor","opened"); 
+   client.publish("outDoor","opened");
    delay(1000);
    digitalWrite(buzzer, LOW);
    delay(5000);
-   digitalWrite(relay,LOW); 
-    
-    
+   digitalWrite(relay,LOW);
+
+
   }
    else {
     digitalWrite(relay, LOW);
@@ -102,7 +170,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
      client.publish("outDoor","closed");
    }
 }
-   
+
  void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
@@ -128,18 +196,28 @@ void callback(char* topic, byte* payload, unsigned int length) {
 }
 void setup(){
   Serial.begin(9600);
+  //#ifdef ESP8266
+    botclient.setInsecure();
+ //#endif
   input_password.reserve(32); // maximum input characters is 33, change if needed
-  
+   configTime(0, 0, "pool.ntp.org");
    debugln("\n\nStarting program");
    setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 
-  
+
    pinMode(relay,OUTPUT);
    pinMode(buzzer,OUTPUT);
-   digitalWrite(relay,LOW);
+   //digitalWrite(relay,LOW);
 }
+ // WiFi.mode(WIFI_STA);
+  //WiFi.begin(ssid, password);
+
+ // while (WiFi.status() != WL_CONNECTED) {
+    //delay(1000);
+    //Serial.println("Connecting to WiFi..");
+  //}
 
 void loop() {
   char key = keypad.getKey();
@@ -155,7 +233,7 @@ void loop() {
         digitalWrite(relay,HIGH);
         delay(3000);
         digitalWrite(relay,LOW);
-        client.publish("outDoor","opened"); 
+        client.publish("outDoor","opened");
 
       } else {
         Serial.println("The password is incorrect, ACCESS DENIED!");
@@ -163,7 +241,7 @@ void loop() {
          digitalWrite(buzzer, HIGH);
          delay(500);
          digitalWrite(buzzer,LOW);
-         client.publish("outDoor","closed"); 
+         client.publish("outDoor","closed");
       }
 
       input_password = ""; // clear input password
@@ -177,5 +255,14 @@ if (!client.connected())  // Reconnect if connection is lost
     reconnect();
   }
   client.loop();
+  if (millis() > lastTimeBotRan + botRequestDelay)  {
+    int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    while(numNewMessages) {
+      Serial.println("got response");
+      handleNewMessages(numNewMessages);
+      numNewMessages = bot.getUpdates(bot.last_message_received + 1);
+    }
+    lastTimeBotRan = millis();
+  }
 
 }
